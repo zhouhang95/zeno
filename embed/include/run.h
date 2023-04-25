f980f70211a95c7225ef802f3da4428ccf5470d1
DarcJC <me@darc.pro>
2023-04-25 16:46:30 +0800

    feat: embed example

---
+++ embed/include/run.h
@@ -0,0 +1,68 @@
+#ifndef ZENO_EMBED_RUN_H
+#define ZENO_EMBED_RUN_H
+
+#include "cobalt.h"
+
+#include <iostream>
+#include <fstream>
+#include <memory>
+#include <string>
+#include <zeno/core/Session.h>
+#include <zeno/core/Graph.h>
+
+class RunCommand : public Cobalt::Command<RunCommand> {
+public:
+  static std::string Use() {
+      return "run";
+  }
+
+  static std::string Short() {
+      return "Run [ zsl file ].";
+  }
+
+  static std::string Long() {
+      return "Run the given zsl file.";
+  }
+
+  void RegisterFlags() {
+      AddLocalFlag<bool>(bUseFileInput, "with_file", "f", true, "Read zsl from file.");
+  }
+
+  int Run(const Cobalt::Arguments& Args) {
+      // Read file
+      if (bUseFileInput && !Args.empty()) {
+          const std::string& FilePath = Args[0];
+          std::freopen(FilePath.c_str(), "r", stdin);
+      }
+
+      // Loading json from stdin buffer
+      std::ostringstream StringStream;
+      StringStream << std::cin.rdbuf();
+      std::string InJson { StringStream.str() };
+
+      // Create new graph
+      std::shared_ptr<zeno::Graph> NewGraph = zeno::getSession().createGraph();
+      NewGraph->loadGraph(InJson.c_str());
+
+      // Run graph
+      NewGraph->applyNodesToExec();
+      try {
+      } catch (...) {
+          std::cerr << "There is an exception happened." << std::endl;
+      }
+
+      return 0;
+  }
+
+private:
+  bool bUseFileInput;
+};
+
+class RootCommand : public Cobalt::Command<RootCommand, RunCommand> {
+public:
+  static std::string Use() {
+      return "zeno";
+  }
+};
+
+#endif //ZENO_EMBED_RUN_H
--- embed/main.cpp
+++ embed/main.cpp
@@ -1,11 +1,15 @@
-#include <zeno/core/Session.h>
-#include <zeno/core/Graph.h>
 
-int main() {
-    const char *json = "json here";
-    auto g = zeno::getSession().createGraph();
-    g->addSubnetNode("custom")->loadGraph(json);
-    std::map<std::string, std::shared_ptr<zeno::IObject>> inputs, outputs;
-    outputs = g->callSubnetNode("custom", std::move(inputs));
-    return 0;
+#include "include/cobalt.h"
+#include "include/run.h"
+
+int main(int argc, char** argv) {
+    // Parse input args
+    return Cobalt::Execute<RootCommand>(argc, argv);
+
+//    const char *json = "json here";
+//    auto g = zeno::getSession().createGraph();
+//    g->addSubnetNode("custom")->loadGraph(json);
+//    std::map<std::string, std::shared_ptr<zeno::IObject>> inputs, outputs;
+//    outputs = g->callSubnetNode("custom", std::move(inputs));
+//    return 0;
 }
